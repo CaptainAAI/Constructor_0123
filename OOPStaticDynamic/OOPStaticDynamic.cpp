cf048d0f73eb2a7fa268c8098cc92e9f806803c7
CaptainAAI <anandaagungismail@gmail.com>
2024-05-13 10:42:13 +0700

    fix the number for mhs.printall code

--- OOPStaticDynamic/OOPStaticDynamic.cpp
+++ OOPStaticDynamic/OOPStaticDynamic.cpp
@@ -39,9 +39,9 @@ int main()
 	mahasiswa mhs4("Joko Purbo");
 
 	mhs1.printAll();
-	mhs1.printAll();
-	mhs1.printAll();
-	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
 	return 0;
 }
 
